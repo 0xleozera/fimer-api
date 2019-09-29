b91563d02b971467181780a8a7db070dd8bc8c2e
Leonardo Pinheiro <leonardo.pinheiro@proenem.com>
2019-09-29 01:35:20 -0300

    add (db) config

--- config/database.js
+++ config/database.js
@@ -75,8 +75,8 @@ module.exports = {
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
+      user: Env.get('DB_USER', 'postgres'),
+      password: Env.get('DB_PASSWORD', 'postgres'),
       database: Env.get('DB_DATABASE', 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
