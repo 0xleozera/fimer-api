100684bfc50f5ac209c66c166cd00004d3916a5b
Leonardo Pinheiro <leoprocha100@gmail.com>
2019-09-29 04:37:27 -0300

    add (files) routes

--- start/routes.js
+++ start/routes.js
@@ -4,3 +4,10 @@ const Route = use('Route')
 
 Route.post('users', 'UserController.store')
 Route.post('auth', 'AuthController.store')
+
+Route.get('files/:id', 'FileController.show')
+Route.post('files', 'FileController.store')
+
+Route.group(() => {
+
+}).middleware(['auth'])
