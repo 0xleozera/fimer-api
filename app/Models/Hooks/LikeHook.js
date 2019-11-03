52285f2e8a8589d41d4b80862c504730dea22ece
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-03 03:25:45 -0300

    fix (like) logic of hook

--- app/Models/Hooks/LikeHook.js
+++ app/Models/Hooks/LikeHook.js
@@ -24,11 +24,10 @@ LikeHook.match = async modelInstance => {
 
   const hasLiker =
     liker[0].likees.filter(likee => likee.liker_id === modelInstance.likee_id)
-      .length > 0
+      .length === 1
   const hasLikee =
     likee[0].likees.filter(likee => likee.liker_id === modelInstance.liker_id)
-      .length > 0
-
+      .length === 1
   if (hasLiker && hasLikee) {
     await Match.create({
       matcher_id: modelInstance.liker_id,
