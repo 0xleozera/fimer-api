dfa6be7ce79166214e4e863e4357db6fc390595e
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-04 00:16:21 -0300

    add (like) remove like and match

--- app/Controllers/Http/LikeController.js
+++ app/Controllers/Http/LikeController.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const Database = use('Database')
 const Like = use('App/Models/Like')
+const Match = use('App/Models/Match')
 
 class LikeController {
   async store ({ request, auth }) {
@@ -9,6 +11,48 @@ class LikeController {
     const like = await Like.create({ liker_id: auth.user.id, ...data })
 
     return like
+  }
+
+  async destroy ({ params, response, auth }) {
+    const trx = await Database.beginTransaction()
+
+    await Like.query()
+      .where('liker_id', auth.user.id)
+      .andWhere('likee_id', params.likee_id)
+      .delete(trx)
+
+    const fetchMatch = await Match.query()
+      .where(builder => {
+        builder
+          .where('matcher_id', auth.user.id)
+          .orWhere('matchee_id', auth.user.id)
+      })
+      .andWhere(builder => {
+        builder
+          .where('matchee_id', params.likee_id)
+          .orWhere('matcher_id', params.likee_id)
+      })
+      .fetch()
+    const [match] = fetchMatch.toJSON()
+
+    if (match) {
+      await await Match.query()
+        .where(builder => {
+          builder
+            .where('matcher_id', auth.user.id)
+            .orWhere('matchee_id', auth.user.id)
+        })
+        .andWhere(builder => {
+          builder
+            .where('matchee_id', params.likee_id)
+            .orWhere('matcher_id', params.likee_id)
+        })
+        .delete(trx)
+    }
+
+    await trx.commit()
+
+    return response.status(200).send({ message: 'Jogar removido com sucesso!' })
   }
 }
 
