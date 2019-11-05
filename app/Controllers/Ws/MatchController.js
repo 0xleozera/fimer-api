cb30d04f0eaec30d5ed8d5e742a3ac4983d33bd4
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-05 20:31:17 -0300

    fix (messages and matches) websocket

--- app/Controllers/Ws/MatchController.js
+++ app/Controllers/Ws/MatchController.js
@@ -4,13 +4,6 @@ class MatchController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
-
-    const [, id] = this.socket.topic.split(':')
-    const { matchee_id, matcher_id } = this.request.post()
-
-    if (Number(id) !== matchee_id && Number(id) !== matcher_id) {
-      this.socket.close()
-    }
   }
 }
 
--- app/Controllers/Ws/MessageController.js
+++ app/Controllers/Ws/MessageController.js
@@ -4,13 +4,6 @@ class MessageController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
-
-    const [, id] = this.socket.topic.split(':')
-    const { match_id } = this.request.post()
-
-    if (Number(id) !== match_id) {
-      this.socket.close()
-    }
   }
 }
 
