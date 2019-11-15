abe2787b529a400a669b74b50859e7dc15c1179d
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-15 14:32:35 -0300

    fix

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -1,49 +1,49 @@
-'use strict'
+"use strict";
 
-const User = use('App/Models/User')
+const User = use("App/Models/User");
 
 class UserSeeder {
-  async run () {
+  async run() {
     await User.create({
-      name: 'Leonardo Pinheiro',
-      nickname: 'Darfus',
-      email: 'admin@admin.com',
-      gender: 'Masculino',
-      region: 'Rio Grande do Sul',
-      birth_date: '06/05/1999',
-      password: '123123'
-    })
+      name: "Leonardo Pinheiro",
+      nickname: "Darfus",
+      email: "admin@admin.com",
+      gender: "Masculino",
+      region: "Rio Grande do Sul",
+      birth_date: "06/05/1999",
+      password: "123123"
+    });
 
     await User.create({
-      name: 'Vitória Alves',
-      nickname: 'Vicaz',
-      email: 'vicaz@admin.com',
-      gender: 'Feminino',
-      region: 'Rio Grande do Sul',
-      birth_date: '23/09/1997',
-      password: '123123'
-    })
+      name: "Vitória Alves",
+      nickname: "Vicaz",
+      email: "vicaz@admin.com",
+      gender: "Feminino",
+      region: "Rio Grande do Sul",
+      birth_date: "23/09/1997",
+      password: "123123"
+    });
 
     await User.create({
-      name: 'Guilherme Monteiro',
-      nickname: 'Shouden',
-      email: 'gui@admin.com',
-      gender: 'Masculino',
-      region: 'Rio Grande do Sul',
-      birth_date: '31/01/1998',
-      password: '123123'
-    })
+      name: "Guilherme Monteiro",
+      nickname: "Shouden",
+      email: "gui@admin.com",
+      gender: "Masculino",
+      region: "Rio Grande do Sul",
+      birth_date: "31/01/1998",
+      password: "123123"
+    });
 
     await User.create({
-      name: 'Mateus Ribeiro',
-      nickname: 'Hakeu',
-      email: 'mathy@admin.com',
-      gender: 'Masculino',
-      region: 'São Paulo',
-      birth_date: '16/12/1998',
-      password: '123123'
-    })
+      name: "Mateus Ribeiro",
+      nickname: "Hakeu",
+      email: "mathy@admin.com",
+      gender: "Masculino",
+      region: "São Paulo",
+      birth_date: "16/12/1998",
+      password: "123123"
+    });
   }
 }
 
-module.exports = UserSeeder
+module.exports = UserSeeder;
