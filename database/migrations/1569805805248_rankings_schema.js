7546e017ecb29f4bc6cdd3ecfcac82f1b72f1262
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-19 02:24:57 -0300

    fix (ranking) slug

--- database/migrations/1569805805248_rankings_schema.js
+++ database/migrations/1569805805248_rankings_schema.js
@@ -1,14 +1,21 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
 class RankingsSchema extends Schema {
   up () {
-    this.create('rankings', (table) => {
+    this.create('rankings', table => {
       table.increments()
       table.string('description', 80).notNullable()
-      table.integer('game_id').unsigned().references('id').inTable('games').onUpdate('CASCADE').notNullable()
+      table.string('slug', 40)
+      table
+        .integer('game_id')
+        .unsigned()
+        .references('id')
+        .inTable('games')
+        .onUpdate('CASCADE')
+        .notNullable()
       table.timestamps()
     })
   }
