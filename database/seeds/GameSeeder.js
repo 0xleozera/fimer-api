78b7aaaee216b8c6884cc010c2a7eedec450cb61
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-24 01:52:12 -0300

    add (project) all seeds

--- database/seeds/GameSeeder.js
+++ database/seeds/GameSeeder.js
@@ -5,6 +5,9 @@ const Game = use('App/Models/Game')
 class GameSeeder {
   async run () {
     await Game.create({ name: 'League of Legends', slug: 'LOL' })
+    await Game.create({ name: 'Fortnite', slug: 'FORT' })
+    await Game.create({ name: 'Counter-Strike', slug: 'CSGO' })
+    await Game.create({ name: 'Dota2', slug: 'DOTA' })
   }
 }
 
